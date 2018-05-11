40080aa1e005bbe329553eecf1f7ef572bb37da9
jajibhee <jajmantobi@gmail.com>
2018-05-11 13:42:54 +0100

    Jajman added files

---
+++ hello.php
@@ -0,0 +1,185 @@
+<!doctype html>
+<html lang="en">
+    
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Imani | Tolu Faith - HNG 4.O</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,800" rel="stylesheet">
+        <!-- <link rel="stylesheet" href="css/normalize.css"> -->
+        <link rel="stylesheet" href="css/style.css">
+        <!-- <link rel="stylesheet" href="css/bootstrap.css"> -->
+        <link href="https://cdn.jsdelivr.net/npm/pretty-checkbox@3.0/dist/pretty-checkbox.min.css" rel="stylesheet">
+        <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
+        
+    </head>
+    <body>
+     
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-sm-12 col-xl-12">
+                    <div class="ini-div">
+                    <?php
+                        echo "The time is " . date("h:i:sa");
+                    ?>		
+                    </div>
+                </div>
+            </div>        
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-sm-12 col-xl-12">
+                    <div class="first-div">
+                        <h1 class="text-h1">Imani</h1>			
+                    </div>
+                </div>
+            </div>        
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-sm-12 col-xl-12">
+                    <div class="second-div">
+                        <p class="text-h2">HNG 4.0 - Hotels NG Internship</p>			
+                    </div>
+                </div>
+            </div>        
+        </div>
+			
+            
+            <div class="third-div">
+                    <p class="text-3">Find and book hotels in Nigeria.</p>
+            </div>
+            <div class="task">
+                    <p class="text-4">Tasks</p>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-xl-12">
+                            <div class="task-details pretty p-bigger p-pulse p-jelly p-svg p-curve">
+                                <input type="checkbox" />
+                                    <div class="state p-primary ">
+                                        <!-- svg path -->
+                                        <svg class="svg svg-icon" viewBox="0 0 20 20">
+                                            <path d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z" style="stroke: white;fill:white;"></path>
+                                        </svg>
+                                        <label><span class="text-5">Design a very simple page in figma</span> </label>
+                                    </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-xl-12">
+                        <div class="task-details pretty p-bigger p-pulse p-jelly p-svg p-curve">
+                            <input type="checkbox" />
+                                <div class="state p-primary ">
+                                    <!-- svg path -->
+                                    <svg class="svg svg-icon" viewBox="0 0 20 20">
+                                        <path d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z" style="stroke: white;fill:white;"></path>
+                                    </svg>
+                                    <label><span class="text-5">Implement it in  HTML/CSS  ( this design should have space for showing
+                                            the curent time.)</span> </label>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-xl-12">
+                        <div class="task-details pretty p-bigger p-pulse p-jelly p-svg p-curve">
+                            <input type="checkbox" />
+                                <div class="state p-primary ">
+                                    <!-- svg path -->
+                                    <svg class="svg svg-icon" viewBox="0 0 20 20">
+                                        <path d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z" style="stroke: white;fill:white;"></path>
+                                    </svg>
+                                    <label><span class="text-5"> Setup docker.</span> </label>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-xl-12">
+                        <div class="task-details pretty p-bigger p-pulse p-jelly p-svg p-curve">
+                            <input type="checkbox" />
+                                <div class="state p-primary ">
+                                    <!-- svg path -->
+                                    <svg class="svg svg-icon" viewBox="0 0 20 20">
+                                        <path d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z" style="stroke: white;fill:white;"></path>
+                                    </svg>
+                                    <label><span class="text-5"> Get PHP to run in docker.</span> </label>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-xl-12">
+                        <div class="task-details pretty p-bigger p-pulse p-jelly p-svg p-curve">
+                            <input type="checkbox" />
+                                <div class="state p-primary ">
+                                    <!-- svg path -->
+                                        <svg class="svg svg-icon" viewBox="0 0 20 20">
+                                            <path d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z" style="stroke: white;fill:white;"></path>
+                                        </svg>
+                                        <label><span class="text-5"> Your app must show properly.</span> </label>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-xl-12">
+                        <div class="task-details pretty p-bigger p-pulse p-jelly p-svg p-curve">
+                            <input type="checkbox" />
+                                <div class="state p-primary ">
+                                    <!-- svg path -->
+                                        <svg class="svg svg-icon" viewBox="0 0 20 20">
+                                                <path d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z" style="stroke: white;fill:white;"></path>
+                                            </svg>
+                                            <label><span class="text-5"> You will need to send a screenshot to pass to stage 2.</span> </label>
+                                    </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        <footer>
+                    
+            <div class="container">
+                    
+                <div class="row">
+                        
+                    <div class="col-md-12">
+                        
+                        <p>© 2018 <a href="index.html">Tolu Faith</a>. All rights reserved.</p>
+                        
+                    </div>
+                        
+                </div>
+                    
+        </div>
+                    
+    </footer>
+
+			
+    </body>
+
+
+
+</html>
